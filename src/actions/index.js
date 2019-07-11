79e25f9210a873442753cde91f6d6582d4e81d89
Andrey Safonov <a.so777@live.ca>
2019-07-10 20:42:42 -0700

    added memoization to reduce number of calls to the API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -9,11 +10,15 @@ export const fetchPosts = () => async dispatch => {
     });
 };
 
-export const fetchUser = (userId) => async dispatch => {
+export const fetchUser = (userId) => dispatch => {
+    _fetchUser(userId, dispatch);
+};
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${userId}`);
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     });
-};
+});
